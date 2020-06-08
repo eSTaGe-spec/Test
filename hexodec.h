8493feb6b99c22d0a9c6149e2199b65731c97825
eSTaGe-spec <63460844+eSTaGe-spec@users.noreply.github.com>
2020-06-08 18:17:01 +0300

    Create hexodec.h

---
+++ hexodec.h
@@ -0,0 +1,13 @@
+#ifndef HEXODEC_H
+#define HEXODEC_H
+#include <iostream>
+#include <string>
+#include <math.h>
+#include <cstdlib>
+
+using namespace std;
+
+	void FromHexToDecimale(string str, int *NumberDec);
+	void FromDecimaleToHex(int NumberDec, string *str);
+	void FromHexToBin(string *s);
+#endif
