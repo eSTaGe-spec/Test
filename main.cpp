56de90bd495b1ee8c961afd192c3c277ae91aab0
eSTaGe-spec <63460844+eSTaGe-spec@users.noreply.github.com>
2020-06-08 18:13:18 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,57 +2,92 @@
 #include <string>
 #include <math.h>
 #include <cstdlib>
-
-
+#include "binadec.h"
+#include "hexodec.h"
+#include "common.h"
 
 //pow-возведение в степень(exponentiation)
 
 using namespace std;
 
-	int BaseFrom;//Из какой системы исчесления
-	int BaseTo;//В какую систему исчесления
-    int NumberFrom[100];
+	int BaseFrom;//Из какой системы исчесления(What number system)
+	int BaseTo;//В какую систему исчесления(Which number system)
     int NumberDec;
-void FromBinaryToDecimale(int NumberFrom[],int counter){ //из 2 в 10
-		NumberDec=0;
-        for(int i=0; i<counter; i++){
-        	NumberDec=NumberDec+NumberFrom[i]*pow(BaseFrom,counter-i-1);
-
-		}
-		cout<<NumberDec<<endl;
-}
+    
+   
 
 
 int main(int argc, char** argv) {
+	char n='y';
 	setlocale(LC_ALL,"Russian");
-	string str;
-	char ch[1];
-	int counter;
-	cout<<"Из какой системы исчесления: ";
+	while (n=='y'){
+	cout<<"Из какой системы исчесления(What number system): ";
 	cin>>BaseFrom;
-	cout<<"В какую систему исчесления: ";
+	cout<<"В какую систему исчесления(Which number system): ";
 	cin>>BaseTo;
-	cout<<"Введите число для перевода: ";
-	cin>>str;
-	counter=0;
-	for(int i=0; i<str.size(); i++){
-		ch[0]=str[i];
-		NumberFrom[i]=atoi(ch);
-		counter++;
-		cout<<counter<<endl;
-		//cout<<NumberFrom[i];
-	}		
-	
-    FromBinaryToDecimale(NumberFrom,counter);
-    cout<<"Что получилось: "<<NumberDec;
-    
+	string s;	
+	cout<<"Введите число для перевода(Enter the number to transfer): ";
+	cin>>s;	
+	switch(BaseFrom){
+		
+		case 2:{
+			switch(BaseTo){
+				case 10: {
+					if (FromBinaryToDecimale(s, &NumberDec)==-1)
+						break;
+					cout<<"Ответ: "<<NumberDec<<endl;					
+					break;
+				}
+				case 16: {
+					FromBinToHex(&s);
+					cout<<"Ответ: "<<s<<endl;
+					break;
+				}
+			}
+			break;
+		}
+		case 10:{
+		string str="";
+			switch(BaseTo){
+				case 2: {
+    				FromDecimaleToBinary(FromStrToDec(s), &str);				
+					break;
+				}
+				
+				case 16: {	
+					FromDecimaleToHex(FromStrToDec(s), &str);		
+					break;
+				}				
+			}
+			cout<<"Ответ: "<<str<<endl;
+			break;
+		}
+		case 16:{
+			switch(BaseTo){
+				case 10: {
+					FromHexToDecimale(s, &NumberDec);
+					cout<<"Ответ: "<<NumberDec<<endl;					
+					break;
+				}
+				case 2: {
+					FromHexToBin(&s);
+					cout<<"Ответ: "<<s<<endl;
+					break;
+				}
+			}	
+			break;						
+		}
+		default:{
+			cout<<"Вы ввели не правильное число"<<endl;
+		}
+
+	}
+	cout<<"Хотите продолжить?(y/n)"<<endl;
+	cin>>n;
+	}
 	return 2007;
+
 }
-
-/*ДЗ
-
-ДЗ*/
-
-
+//ДЗ сделать выдор
 
 
