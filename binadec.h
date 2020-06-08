eceb2566361e38555ee8f19d491571c049ad1065
eSTaGe-spec <63460844+eSTaGe-spec@users.noreply.github.com>
2020-06-08 18:16:16 +0300

    Create binadec.h

---
+++ binadec.h
@@ -0,0 +1,13 @@
+#ifndef BINADEC_H
+#define BINADEC_H
+#include <iostream>
+#include <string>
+#include <math.h>
+#include <cstdlib>
+
+using namespace std;
+
+	int FromBinaryToDecimale(string str, int *NumberDec);
+	void FromDecimaleToBinary(int NumberDec, string *str);
+    void FromBinToHex(string *s);
+#endif
